414b9fbca854f312c08198ba97b601abcd92ce74
javier <nhat1234559@gmail.com>
2025-10-29 22:36:16 +0700

    fix: update nextjs image cdn

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tax-zero.cdn.prismic.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tax-zero-landing.cdn.prismic.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
